3d07f35b62790360efa9fc4a1685867ef186d171
Colin Fraser (LinuxIT) <colin.fraser@linuxit.com>
2015-03-05 19:29:08 +0000

    Updated the hipchat token to the correct version

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # hipchatAPI-python
 
 Python Hipchat API
+
+More information is comming...
+
+Just used for notes at the mo! 
+
+# Hipchat V1 token:
+https://hipchat.com/admin/api
+
+# Hipchat V2 token:
+https://hipchat.com/account/api
+
+# Hipchat per room token:
+https://hipchat.com/rooms/tokens/{room_id}
--- hipchat.py
+++ hipchat.py
@@ -4,13 +4,15 @@
 ###
 
 import json
-import urllib2
+import pycurl
 import pprint
+from StringIO import StringIO
+import urllib2
 
 class Hipchat:
 
 	hipchat_url = "https://api.hipchat.com/v2/"
-	hipchat_header = {'content-type':'application/json'}
+	hipchat_header = {'content-type': 'application/json'}
 
 	def __init__(self, hipchat_token):
 		#
@@ -26,16 +28,22 @@ class Hipchat:
 	# list users
 	##
 	def list_users(self):
-		#
-		# Data to send to hipchat
-		data = json.dumps({'start-index': 0, 'max-results': 100, 'include-guests': false, 'include-deleted': false})
-		#
-		# Build the request
-		request = urllib2.Request(self.hipchat_url+"?auth_token="+self.hipchat_token, data, self.hipchat_header)
-		#
-		# send the request and return the resulting json
-		result = urllib2.urlopen(request)
-		return result.read()
+	
+		#buffer = StringIO()
+		#post_data = {'': 'value'}
+		#c = pycurl.Curl()
+		#c.setopt(c.URL, self.hipchat_url+"user/?auth_token="+self.hipchat_token)
+		#c.setopt(c.HTTPHEADER, self.hipchat_header)
+		#c.setopt(c.WRITEDATA, buffer)
+		#c.perform()
+		#c.close()
+		#return buffer.getvalue()
+		
+		data = json.dumps({'start-index': 0, 'max-results': 100, 'include-guests': 0, 'include-deleted': 0})
+		#request = urllib2.Request(self.hipchat_url+"user?auth_token="+self.hipchat_token, data, self.hipchat_header)
+		request = urllib2.Request(self.hipchat_url+"room?auth_token="+self.hipchat_token)
+		print self.hipchat_url+"room?auth_token="+self.hipchat_token
+		return urllib2.urlopen(request)
 		
 
 #
@@ -43,8 +51,7 @@ class Hipchat:
 conf = json.load(open('hipchat_conf.json'))
 
 test = Hipchat(conf['hipchat_token'])
-result = test.list_users
+res = test.list_users()
 
-pprint.pprint(result)
-
+print res.read()
 
